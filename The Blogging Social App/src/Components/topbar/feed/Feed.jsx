26fcbbbfa743c6f6a58502051492043c190b8760
woodyshekinah <woody.shekinah@gmail.com>
2024-05-25 15:38:22 -0700

    Created the side bar and styled it

---
+++ The Blogging Social App/src/Components/topbar/feed/Feed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './feed.css'
+
+export default function Feed() {
+  return (
+    <div className='feed'>
+        feed
+    </div>
+  )
+}
+
--- The Blogging Social App/src/Pages/home/home.jsx
+++ The Blogging Social App/src/Pages/home/home.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import Topbar from "../../Components/topbar/Topbar";
+import React from "react"
+import Topbar from "../../Components/topbar/Topbar"
+import Sidebar from "../../Components/topbar/sidebar/Sidebar"
+import Feed from "../../Components/topbar/feed/Feed"
+import Rightbar from "../../Components/topbar/rightbar/Rightbar"
+import "./home.css"
+
 
 export default function Home() {
     return (
+        <>
         <Topbar />
+        <div className="homeContainer">
+        <Sidebar/>
+        <Feed />
+        <Rightbar />
+        </div>
+        </>
     )
 }
