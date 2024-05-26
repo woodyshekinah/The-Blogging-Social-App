6373d7c76c7d307d97d70484f68924c81f1860e1
woodyshekinah <woody.shekinah@gmail.com>
2024-05-25 17:48:32 -0700

    Created and styled the right bar

--- The Blogging Social App/src/Components/topbar/rightbar/Rightbar.jsx
+++ The Blogging Social App/src/Components/topbar/rightbar/Rightbar.jsx
@@ -4,7 +4,82 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-           rightbar 
+           <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="../src/images/gift.png" alt="Birthday image" />
+                    <span className="birthdayText"><b>Pola Foster</b> and <b>3 other friends</b> have their birthday today</span>
+                </div>
+                <img className="rightbarAd" src="../src/images/ad.png" alt="Ad image" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="../src/images/person/3.jpeg" alt="profile pic" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Jaden Smith</span>
+                    </li>
+                </ul>
+            </div> 
         </div>
     )
 }
