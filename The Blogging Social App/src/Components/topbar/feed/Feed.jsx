fd2ff61b2e76e04beac56ec4582c9aed96182a0f
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-27 23:21:55 +0300

    props added

--- The Blogging Social App/src/Components/topbar/feed/Feed.jsx
+++ The Blogging Social App/src/Components/topbar/feed/Feed.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import './feed.css'
-import Share from '../../share/Share'
-import Post from '../../post/Post'
-import blogsData from './BlogsData.json'
-
+import "./feed.css";
+import Share from "../../share/Share";
+import Post from "../../post/Post";
+import posts from "../feed/BlogsData.json";
 
 export default function Feed() {
   return (
-    <div className='feed'>
-        <div className="feedWrapper">
-            <Share />
-            
-               <Post /> 
-            
-            
-            {/* <Post />
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            profile={post.profile}
+            username={post.username}
+            time={post.time}
+            comments={post.comments}
+            like={post.like}
+            description={post.description}
+          />
+        ))}
+
+        {/* <Post />
             <Post />
             <Post />
             <Post />
@@ -25,9 +32,22 @@ export default function Feed() {
             <Post />
             <Post />
             <Post /> */}
-              
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
+import PropTypes from "prop-types";
+Feed.propTypes = {
+  username: PropTypes.string.isRequired,
+};
+
+Feed.propTypes = {
+  comments: PropTypes.array,
+  time: PropTypes.array,
+  username: PropTypes.array,
+  profile: PropTypes.array,
+  description: PropTypes.array,
+  like: PropTypes.array,
+  id: PropTypes.array,
+};
